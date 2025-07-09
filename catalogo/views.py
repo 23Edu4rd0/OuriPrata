573cf6f3d17a843bb52820c4705759eda7ec2d2b
23Edu4rd0 <eduardo.vchaves23@gmail.com>
2025-07-09 14:02:13 -0300

    feat: Refactor HTML templates to extend base layout and streamline structure; update views for consistency

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
-# Create your views here.
 def index(request):
     return render(request, 'landing_page/index.html')
 
 def contact(request):
-    template = loader.get_template('landing_page/contact.html')
-    return HttpResponse(template.render())
+    return render(request, 'landing_page/contact.html')
 
 def items(request):
     return HttpResponse('<h1>Just testing, never mind.</h1>')
